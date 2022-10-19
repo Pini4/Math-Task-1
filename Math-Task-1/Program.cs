019ba249b5f1b0940e3f40e0457e263a2afd0018
Pini4 <User@WIN-GA1Q70LI9LP>
2022-10-19 19:16:53 +0300

    Добавьте файлы проекта.

---
+++ Math-Task-1/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double x = 1825;
+        double y = 18;
+        double z = 3298;
+
+        Console.WriteLine($@"Данные :
+x={x} 
+y={y}
+z={z}");
+
+        double w = y / x;
+        double s = Math.Abs(Math.Pow(x, w) - Math.Sqrt(w));
+        double i = y - x;
+        double j = i * ((y + z / i) / (1 + (i * i)));
+        Console.WriteLine($@"Первый ответ равен :
+{s}");
+        Console.WriteLine($@"Второй ответ равен :
+{j}");
+    }
+}
